6683e6735f53d2bfeae5d010c9cf71b64a0a01b5
GU Yiling <justice360@gmail.com>
2014-06-22 13:30:33 +0800

    Update kolor.js
    Add `rebeccapurple`.

--- kolor.js
+++ kolor.js
@@ -272,7 +272,8 @@
         "white": "#ffffff",
         "whitesmoke": "#f5f5f5",
         "yellow": "#ffff00",
-        "yellowgreen": "#9acd32"
+        "yellowgreen": "#9acd32",
+        "rebeccapurple": "#663399" // not in CSS spec yet
     };
 
 
